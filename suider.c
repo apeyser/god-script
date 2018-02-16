ab23920079b6dfe8a44412e3489621a91eb81dcf
Alexander Peyser <a.peyser@fz-juelich.de>
2018-02-16 09:29:20 +0100

    cleanup suider headers

--- suider.c
+++ suider.c
@@ -1,13 +1,8 @@
 #include <unistd.h>
-#include <stdio.h>
 #include <stdlib.h>
-#include <sys/types.h>
 #include <sys/wait.h>
 #include <string.h>
-
 #include <stdio.h>
-#include <sys/types.h>
-#include <unistd.h>
 
 extern char **environ;
 
